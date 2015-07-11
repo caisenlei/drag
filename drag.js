e040ca11a56f9ba2e7569f3be4da5ab8fd5f3632
caisenlei <caisenlei@qq.com>
2015-07-11 23:17:33 +0800

    Update drag.js

--- drag.js
+++ drag.js
@@ -2,14 +2,14 @@
 
 function drag(id){
 	var obj = document.getElementById(id);
-	var disX = 0;
-	var disY = 0;
+	var dis_X = 0;
+	var dis_Y = 0;
 	obj.onmousedown = function(ev){
-		disX = ev.pageX - obj.offsetLeft;
-		disY = ev.pageY - obj.offsetTop;
+		dis_X = ev.pageX - obj.offsetLeft;
+		dis_Y = ev.pageY - obj.offsetTop;
 		document.onmousemove = function(ev){
-			obj.style.left = ev.pageX - disX + 'px';
-			obj.style.top = ev.pageY - disY + 'px';
+			obj.style.left = ev.pageX - dis_X + 'px';
+			obj.style.top = ev.pageY - dis_Y + 'px';
 		};
 		document.onmouseup = function(){
 			document.onmousemove = null;
